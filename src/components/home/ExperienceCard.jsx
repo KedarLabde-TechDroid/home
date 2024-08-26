c63661417baabc8d3dd0ab69e8b35a29a8b4edf2
Kedar Labde <labde.kedar@gmail.com>
2024-08-26 18:42:34 +0530

    message change

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-
-import {
-  Col,
-} from "react-bootstrap";
+import { Col } from 'react-bootstrap';
 
 const ExperienceCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+        <img 
+          className="bg-white mb-3" 
+          src={data.companylogo} 
+          alt="" 
+          style={{ width: '200px', height: '200px', objectFit: 'cover' }} 
+        />
         <p className="lead">
           {data.role}
           <br />
           {data.date}
         </p>
-
       </div>
     </Col>
   );
-}
+};
 
 export default ExperienceCard;
