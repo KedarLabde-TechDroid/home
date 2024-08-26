25328368aaba0d144fb7a53582b263822b5d462a
Kedar Labde <labde.kedar@gmail.com>
2024-08-26 19:11:53 +0530

    leadership bug fixes

--- src/components/home/Leadership.jsx
+++ src/components/home/Leadership.jsx
@@ -7,7 +7,7 @@ const Leadership = ({ heading, message, img, imageSize }) => {
     <Jumbotron
       id="leadership"
       className="m-0"
-      style={{ backgroundColor: "white" }}
+      style={{ backgroundColor: "white" }} // Background color of the Jumbotron
     >
       <h2 className="display-4 pb-5 text-center">{heading}</h2>
       <div className="row">
@@ -19,16 +19,27 @@ const Leadership = ({ heading, message, img, imageSize }) => {
             {img.map((value, index) => {
               return (
                 <Carousel.Item key={index}>
-                  <img
-                    className="d-block w-100"
-                    src={value.img}
-                    alt={value.label} // Updated alt text to be dynamic
-                    style={{ 
-                      width: '100%', 
-                      height: imageSize.height, 
-                      objectFit: 'cover' // Use cover to ensure the image covers the area without distortion
+                  <div 
+                    style={{
+                      width: '100%',
+                      height: imageSize.height,
+                      backgroundColor: '#f8f9fa', // Set fill color or background color
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
                     }}
-                  />
+                  >
+                    <img
+                      src={value.img}
+                      alt={value.label}
+                      style={{ 
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        objectFit: 'cover',
+                        display: 'block'
+                      }}
+                    />
+                  </div>
                   <Carousel.Caption>
                     <h3>{value.label}</h3>
                     <p>{value.paragraph}</p>
